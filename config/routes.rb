e96e921c7e2b80e3d371c0f22ac64e6484ee979c
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2022-11-19 04:47:53 +0000

    [Finish]publicコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+
+
 # 管理者用(admin)
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -7,6 +9,8 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
 namespace :admin do
 end
+
+
 
 # 顧客用(public)
 # URL /customers/sign_in ...
@@ -18,7 +22,24 @@ devise_for :customers,skip: [:passwords], controllers: {
 scope module: :public do
   root to: 'homes#top'
   get "/homes/about" => "homes#about"
+
   resources :items, except: [:destroy]
+
+  get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+  patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw'
+  get "customers/my_page" => "customers#show"
+  get '/customers/information/edit' => 'customers#edit'
+  put 'customer/update' => 'customers#update'
+  patch '/customers/information' => 'customers#update'
+
+  resources :cart_items, except:[:show, :new, :edit]
+  delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+
+  resources :orders, except:[:edit, :update, :destroy]
+  post '/orders/confirm' => 'orders#confirm'
+  get '/orders/complete' => 'orders#complete'
+
+  resources :addresses, except:[:new, :show]
 end
 
 
